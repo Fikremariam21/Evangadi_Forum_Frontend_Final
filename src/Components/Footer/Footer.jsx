c30080d38f64d16bac667b028398904db5487868
Fikremariam Mekonnen <fikremariam2012@gmail.com>
2025-04-06 01:33:12 +0300

    Footer style

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,62 +2,61 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../Images/evangadi-logo.png";
 import "./Footer.css";
-import "@fortawesome/fontAwesome-free/css/all.min.css"; // Import Font Awesome
+import "@fortawesome/fontAwesome-free/css/all.min.css";
 
 function Footer() {
   return (
-    <div className="footer ">
-      <div className="container some row">
-        <div className="footer_logo tp col-sm-12 col-md-4">
-          <img src={logo} alt="Evangadi Logo" />
-          <div className="row ic">
-            <Link
-              className="col"
-              to="https://www.facebook.com/evangaditech"
-              target="_blank"
-            >
-              <i className="fab fa-facebook"></i>
-            </Link>
-            <Link className="col" to="">
-              <i className="fab fa-youtube"></i>
-            </Link>
-            <Link
-              className="col"
-              to="https://www.instagram.com/evangaditech/"
-              target="_blank"
-            >
-              <i className="fab fa-instagram"></i>
-            </Link>
+    <div className="footer">
+      <div className="container ">
+        {/* Change justify-content-center to justify-content-between */}
+        {/* Remove text-md-start */}
+        <div className=" link_container row justify-content-between text-center">
+
+          {/* Remove align-items-md-start */}
+          <div className="col-12 col-md-4 mb-4 mb-md-0">
+            <div className="footer_logo d-flex flex-column align-items-center">
+              <img src={logo} alt="Evangadi Logo" className="mb-3" />
+              <div className="social-icons">
+                <Link to="https://www.facebook.com/evangaditech" target="_blank">
+                  <i className="fab fa-facebook mx-2"></i>
+                </Link>
+                <Link to="">
+                  <i className="fab fa-youtube mx-2"></i>
+                </Link>
+                <Link to="https://www.instagram.com/evangaditech/" target="_blank">
+                  <i className="fab fa-instagram mx-2"></i>
+                </Link>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="col-sm-12 col-md-4 tp">
-          <h5 className="title">Useful Links</h5>
-          <ul className="st">
-            <li>
-              <Link to="/how-it-works">How it Works</Link>
-            </li>
-            <li>
-              <Link to="https://www.evangadi.com/legal/terms/" target="_blank">
-                Terms of Service
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="https://www.evangadi.com/legal/privacy/"
-                target="_blank"
-              >
-                Privacy Policy
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="col-sm-12 col-md-4 tp ">
-          <h5 className="title">Contact Info</h5>
-          <ul className="net">
-            <li>Evangadi Networks</li>
-            <li>support@evangadi.com</li>
-            <li>+1-202-386-2702</li>
-          </ul>
+
+          <div className="col-12 col-md-4 mb-4 mb-md-0">
+            <h5 className="title">Useful Links</h5>
+            <ul className="list-unstyled">
+              <li className="mb-2">
+                <Link to="/how-it-works">How it Works</Link>
+              </li>
+              <li className="mb-2">
+                <Link to="https://www.evangadi.com/legal/terms/" target="_blank">
+                  Terms of Service
+                </Link>
+              </li>
+              <li>
+                <Link to="https://www.evangadi.com/legal/privacy/" target="_blank">
+                  Privacy Policy
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-12 col-md-4">
+            <h5 className="title">Contact Info</h5>
+            <ul className="list-unstyled">
+              <li className="mb-2">Evangadi Networks</li>
+              <li className="mb-2">support@evangadi.com</li>
+              <li>+1-202-386-2702</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
